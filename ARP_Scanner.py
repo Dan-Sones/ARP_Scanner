94ac4175b149e0994ac74304eb31fdb31768e867
Daniel Sones <Daniel.sones@btinternet.com>
2021-03-13 12:52:17 +0000

    It appears I can't spell...

--- ARP_Scanner.py
+++ ARP_Scanner.py
@@ -17,13 +17,13 @@ print(r"""
 """)
 print("\n**************************************\n")
 
-# use re to ensure an ipv4 address is being used
+# use 're' to ensure an ipv4 address is being used
 ip_add_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
 
 while True:
     # Get the address range from the user
     ip_add_range_entered = input(
-        "\nPlease enter the ip adress and range you want to send the ARP request to (e.g. 192.168.1.0/24)\n")
+        "\nPlease enter the IP address and range you want to send the ARP request to (e.g. 192.168.1.0/24)\n")
     # If what the user entered fufills the 're' requirements then print it is valid and break
     if ip_add_range_pattern.search(ip_add_range_entered):
         print(f"{ip_add_range_entered} IS a valid ip range")
