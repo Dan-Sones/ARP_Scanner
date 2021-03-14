868cb92487b81fe4167dc48563060373e0901caa
Daniel Sones <Daniel.sones@btinternet.com>
2021-03-14 22:14:34 +0000

    Formatting

--- ARP_Scanner.py
+++ ARP_Scanner.py
@@ -4,6 +4,7 @@ import scapy.all as scapy
 # provides more string manipulation options to be used when formatting Ipv4 address
 import re
 
+print("******************************************************************")
 print(r"""
            _____  _____     _____                                 
      /\   |  __ \|  __ \   / ____|                                
@@ -11,11 +12,9 @@ print(r"""
    / /\ \ |  _  /|  ___/   \___ \ / __/ _` | '_ \| '_ \ / _ \ '__|
   / ____ \| | \ \| |       ____) | (_| (_| | | | | | | |  __/ |   
  /_/    \_\_|  \_\_|      |_____/ \___\__,_|_| |_|_| |_|\___|_|   
-                                                                  
-                                                                  
-                                         
+                                                              
 """)
-print("\n**************************************\n")
+print("******************************************************************\n")
 
 # use 're' to ensure an ipv4 address is being used
 ip_add_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
@@ -26,7 +25,7 @@ while True:
         "\nPlease enter the IP address and range you want to send the ARP request to (e.g. 192.168.1.0/24)\n")
     # If what the user entered fufills the 're' requirements then print it is valid and break
     if ip_add_range_pattern.search(ip_add_range_entered):
-        print(f"{ip_add_range_entered} IS a valid ip range")
+        print(f"{ip_add_range_entered} IS a valid IP range")
         break
 
 arp_result = scapy.arping(ip_add_range_entered)
